efb50be4534beed478b0d9d2abc506c1d15cd501
saurabh chaddha <saurabhchaddha6@gmail.com>
2024-03-01 09:50:43 +0530

    first

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+app.post('api/v1/user/signup', (c) => {
+  return c.text("Signup")
+})
+
+app.post('api/v1/user/signin', (c) => {
+  return c.text("Signin")
+})
+
+app.post('api/v1/blog', (c) => {
+  return c.text("Blog")
+})
+
+app.put('api/v1/blog', (c) => {
+  return c.text("Signup")
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
